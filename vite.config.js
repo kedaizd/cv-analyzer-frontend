5a5518bf01418459fb42dbe4055d10cee381e885
Paw Dz <kontofejsik@gmail.com>
2025-08-13 17:43:01 +0200

    zmiana vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './',
-server: {
+  base: '/', // Zmień na "/" zamiast "./"
+  server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // backend lokalny
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
         changeOrigin: true
       }
     }
